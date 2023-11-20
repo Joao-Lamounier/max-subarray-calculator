61f86859e1d305dc7f654669729c1e0e921d26bc
Joao-Lamounier <112670078+Joao-Lamounier@users.noreply.github.com>
2023-11-20 18:07:24 -0300

    fix no somatorio

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import argparse
 
-
-def set_sum(init, final, interval, arr):
+def set_sum(start, end, step, arr):
     current_sum = 0
     max_sum = float('-inf')
-    for i in range(init, final - 1, interval):
+    for i in range(start, end, step):
         current_sum += arr[i]
         max_sum = max(max_sum, current_sum)
     return max_sum
-
 
 def max_subarray_divide_conquer(arr, low, high):
     if low == high:
@@ -19,25 +17,22 @@ def max_subarray_divide_conquer(arr, low, high):
     left_max = max_subarray_divide_conquer(arr, low, mid)
     right_max = max_subarray_divide_conquer(arr, mid + 1, high)
 
-    left_sum = set_sum(mid, low, -1, arr)
-    right_sum = set_sum(mid + 1, high + 1, +1, arr)
+    left_sum = set_sum(mid, low - 1, -1, arr)
+    right_sum = set_sum(mid + 1, high + 1, 1, arr)
     cross_max = left_sum + right_sum
 
     return max(left_max, right_max, cross_max)
 
-
 def find_max_subarray(arr):
     return max_subarray_divide_conquer(arr, 0, len(arr) - 1)
-
 
 def init_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Max Subarray Calculator')
     parser.add_argument('--array', '-a', type=int, nargs='*', help='Array', required=True)
     return parser.parse_args()
 
-
 if __name__ == "__main__":
     args = init_args()
     arr = args.array
     result = find_max_subarray(arr)
-    print("A soma do maior subconjunto é: ", result)
+    print("A soma do maior subconjunto é:", result)
